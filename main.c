d34a38ea30a471aaed3a5bdaa8180f1d766c3ca4
Eleonora Rezza <eleonora.rezza@mail.polimi.it>
2025-07-31 00:54:38 +0200

    few changes

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 /*
  *	Struttura delle tile: per ogni esagono della mappa vengono salvate le cordinate delle x, delle y, il costo di
  *	transito e una lista concatenata per gestire le rotte aeree.
+ *	Nella tile ho deciso di salvare le cordinate cubiche della tessera. dalla matrice map si accede alla tile con
+ *	le cordinate con la convenzione dell'utente, queste verranno trasformate in cubiche per i calcoli e le ricerche.
  */
 
 //================= TILE =================//
@@ -11,59 +13,82 @@ typedef struct airRoute {
 	int xDest;
 	int yDest;
 	int costAirRoute;
-	struct airRoute* next;
-}AirRoute;
+	struct airRoute *next;
+} AirRoute;
 
 typedef struct tile {
 	int x;
 	int y;
+	int z;
 	int cost;
 	int numAirRoute;
-	AirRoute* array;
+	AirRoute *array;
 	int routeAdded;
-}Tile;
+} Tile;
+
 //========================================//
 
-int AirRouteCost (Tile** map, int x, int y);
+int AirRouteCost(Tile **map, int x, int y);
 
-int main(){
+int main() {
 	char command[64];
 	int res = 0, row = 0, col = 0;
-	Tile** map = NULL;
+	Tile **map = NULL;
 
 	while (res != EOF) {
 		res = scanf("%s", command);
 		switch (command) {
 			case 'init':
+				//se la mappa è già presente la si libera
+				if (map != NULL) {
+					for (int i = 0; i < row; ++i) {
+						free(map[i]);
+					}
+					free(map);
+					map = NULL;
+				}
+
+				//poi si procede nella lettura dei valori e alla crazione di una nuova mappa
 				res = scanf("%d", &col);
 				res = scanf("%d", &row);
-				map = malloc(row * sizeof(Tile*));
+
+				map = malloc(row * sizeof(Tile *));
 				for (int i = 0; i < row; i++) {
-					map[i] = (Tile*) malloc(col * sizeof(Tile));
+					map[i] = (Tile *) malloc(col * sizeof(Tile));
 					for (int j = 0; j < col; j++) {
-						map[i][j].x = i;
-						map[i][j].y = j;
+						map[i][j].x = j - ((i - ( i & 1 )) / 2);
+						map[i][j].z = i;
+						map[i][j].y = - map[i][j].x - map[i][j].z;
 						map[i][j].cost = 1;
+						map[i][j].numAirRoute = 0;
+						map[i][j].array = NULL;
 					}
 				}
 				printf("OK\n");
 				break;
 			case 'change_cost':
+				int x, y, v, raggio;
+				res = scanf("%d" "%d" "%d" "%d", &x, &y, &v, &raggio);
+				if (x < row && x >= 0 && y < col && y >= 0 && raggio > 0) {
+
+				}
+				else {
+					printf("KO\n");
+				}
 				break;
 			case 'toggle_air_route':
 				int flag = 0; //flag per la ricerca della rotta da inserire
 				int x_start = 0, y_start = 0, x_end = 0, y_end = 0;
-				res = scanf("%d" "%d" "%d" "%d" , x_start, y_start, &x_end, &y_end);
+				res = scanf("%d" "%d" "%d" "%d", x_start, y_start, &x_end, &y_end);
 
 				if (x_start <= row && x_end <= row && y_end <= col && y_end <= col) {
-					AirRoute* airRouteHead = map[x_start][y_start].array;
-					AirRoute* airRoutePrev = NULL;
+					AirRoute *airRouteHead = map[x_start][y_start].array;
+					AirRoute *airRoutePrev = NULL;
 
 					while (airRouteHead != NULL || flag == 1) {
 						if (airRouteHead->xDest == x_end && airRouteHead->yDest == y_end) {
 							flag = 1;
-						}
-						else {
+						} else {
 							airRoutePrev = airRouteHead;
 							airRouteHead = airRouteHead->next;
 						}
@@ -72,32 +97,30 @@ int main(){
 					//se lo trova, lo rimuove
 					if (flag == 1 && airRoutePrev != NULL) {
 						airRoutePrev->next = airRouteHead->next;
-						map[x_start][y_start].numAirRoute --;
+						map[x_start][y_start].numAirRoute--;
 						printf("OK\n");
-					}
-					else if (flag == 1 && airRoutePrev == NULL) {
+					} else if (flag == 1 && airRoutePrev == NULL) {
 						map[x_start][y_start].array = airRouteHead->next;
-						map[x_start][y_start].numAirRoute --;
+						map[x_start][y_start].numAirRoute--;
 						printf("OK\n");
 					}
 					//altrimenti lo aggiunge solo se ce ne sono meno di 5
 					else if (map[x_start][y_start].numAirRoute < 5) {
-						AirRoute* airRouteNew = malloc(sizeof (AirRoute));
+						AirRoute *airRouteNew = malloc(sizeof(AirRoute));
 						airRouteNew->xDest = x_end;
 						airRouteNew->yDest = y_end;
 						airRouteNew->costAirRoute = AirRouteCost(map, x_start, y_start);
+						map[x_start][y_start].numAirRoute++;
 						airRouteNew->next = map[x_start][y_start].array;
 						map[x_start][y_start].array = airRouteNew;
 						printf("OK\n");
-					}
-					else {
+					} else {
 						printf("KO\n");
 					}
 
-					free (airRoutePrev);
+					free(airRoutePrev);
 					free(airRouteHead);
-				}
-				else {
+				} else {
 					printf("KO\n");
 				}
 
@@ -111,11 +134,11 @@ int main(){
 	free(map);
 }
 
-int AirRouteCost (Tile** map, int x, int y) {
+int AirRouteCost(Tile **map, int x, int y) {
 	int sum = 0;
 	for (int i = 0; i < map[x][y].numAirRoute; i++) {
 		sum = sum + map[x][y].array[i].costAirRoute;
 	}
-	sum = (sum + map[x][y].cost)/( map[x][y].numAirRoute +1);
+	sum = (sum + map[x][y].cost) / (map[x][y].numAirRoute + 1);
 	return sum;
 }
