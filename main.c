bdcab380c2b296d28e2c3cef129c3b2c4be3c07f
Eleonora Rezza <eleonora.rezza@mail.polimi.it>
2025-08-21 22:24:48 +0200

    Ended "travel cost" with Dijkstra. Not working for now

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
  *	Struttura delle tile: per ogni esagono della mappa vengono salvate le cordinate delle x, delle y, il costo di
@@ -32,6 +33,7 @@ typedef struct tile {
 typedef struct heap {
 	int key;
 	int id;
+	int prev;
 } Heap;
 //========================================//
 
@@ -46,167 +48,160 @@ void Swap (Heap *a, Heap *b);
 void DecreaseKey (Heap *Q, int i, int key);
 int Parent(int i);
 void HeapInsert (Heap *Q, int *heapSize, int key);
+void DijkstraShortestPath(Tile **G, int w, int id, int col, int row);
 
 int main() {
 	char command[64];
 	int res = 0, row = 0, col = 0;
 	Tile **map = NULL;
 
-	while (res != EOF) {
-		res = scanf("%s", command);
-		switch (command) {
-			case 'init':
-				//se la mappa è già presente la si libera
-				if (map != NULL) {
-					for (int i = 0; i < row; ++i) {
-						free(map[i]);
-					}
-					free(map);
-					map = NULL;
+	while ((res = scanf("%s", command)) != EOF) {
+		if (strcmp(command, "init") == 0) {
+			//se la mappa è già presente la si libera
+			if (map != NULL) {
+				for (int i = 0; i < row; ++i) {
+					free(map[i]);
 				}
+				free(map);
+				map = NULL;
+			}
 
-				//poi si procede nella lettura dei valori e alla crazione di una nuova mappa
-				res = scanf("%d", &col);
-				res = scanf("%d", &row);
+			//poi si procede nella lettura dei valori e alla crazione di una nuova mappa
+			scanf("%d", &col);
+			scanf("%d", &row);
 
-				map = malloc(row * sizeof(Tile *));
-				for (int i = 0; i < row; i++) {
-					map[i] = (Tile *) malloc(col * sizeof(Tile));
-					for (int j = 0; j < col; j++) {
-						map[i][j].x = j - ((i - ( i & 1 )) / 2);
-						map[i][j].z = i;
-						map[i][j].y = - map[i][j].x - map[i][j].z;
-						map[i][j].cost = 1;
-						map[i][j].numAirRoute = 0;
-						map[i][j].array = NULL;
-					}
+			map = malloc(row * sizeof(Tile *));
+			for (int i = 0; i < row; i++) {
+				map[i] = (Tile *) malloc(col * sizeof(Tile));
+				for (int j = 0; j < col; j++) {
+					map[i][j].x = j - ((i - ( i & 1 )) / 2);
+					map[i][j].z = i;
+					map[i][j].y = - map[i][j].x - map[i][j].z;
+					map[i][j].cost = 1;
+					map[i][j].numAirRoute = 0;
+					map[i][j].array = NULL;
 				}
-				printf("OK\n");
-				break;
-			case 'change_cost':
-				int x, y, v, raggio, hColo, val;
-				res = scanf("%d" "%d" "%d" "%d", &x, &y, &v, &raggio);
-				if (x < row && x >= 0 && y < col && y >= 0 && raggio > 0 && -10 <= v && v >= 10) {
-					hColo = 1;
-					//loop per le colonne da modificare (asse x)
-					for (int i = -raggio; i <= raggio; i++) {
-						//loop per le righe da modificare (asse y)
-						if ((x+i) >= 0 && (x+i) < col) { //solo se esiste la colonna, risparimo tempo
-							for (int j = -hColo; j <= hColo; j++) {
-								if ((y+j) >= 0 && (y+j) < row) { // solo se esiste la riga
-									val = Incremento(map, x+i, y+j, v, raggio, DistanzaEsagoni(map, x+i, y+j, x, y));
-									if (val > 0 && val <= 100) { //lo aggiorna solo se il nuovo costo (finale) è tra 0 e 100
-										map[x+i][y+j].cost = val;
-										for (int k = 0; k < map[x+i][y+j].numAirRoute; k++) {
-											map[x+i][y+j].array[k].costAirRoute = val;
-										}
-									}else {
-										if (val <= 0) { // se è negativo lo poni a zero
-											map[x+i][y+j].cost = 0;
-											for (int k = 0; k < map[x+i][y+j].numAirRoute; k++) {
-												map[x+i][y+j].array[k].costAirRoute = 0;
-											}
-										}
+			}
+			printf("OK\n");
+		}
+	else if (strcmp(command, "change_cost") == 0) { //! controlla perchè raggio 1 è solo la cella inserita
+		int x, y, v, raggio, hColo, val;
+		scanf("%d" "%d" "%d" "%d", &x, &y, &v, &raggio);
+		if (x < row && x >= 0 && y < col && y >= 0 && raggio > 0 && -10 <= v && v >= 10) {
+			hColo = 1;
+			//loop per le colonne da modificare (asse x)
+			for (int i = -raggio; i <= raggio; i++) {
+				//loop per le righe da modificare (asse y)
+				if ((x+i) >= 0 && (x+i) < col) { //solo se esiste la colonna, risparimo tempo
+					for (int j = -hColo; j <= hColo; j++) {
+						if ((y+j) >= 0 && (y+j) < row) { // solo se esiste la riga
+							val = Incremento(map, x+i, y+j, v, raggio, DistanzaEsagoni(map, x+i, y+j, x, y));
+							if (val > 0 && val <= 100) { //lo aggiorna solo se il nuovo costo (finale) è tra 0 e 100
+								map[x+i][y+j].cost = val;
+								for (int k = 0; k < map[x+i][y+j].numAirRoute; k++) {
+									map[x+i][y+j].array[k].costAirRoute = val;
+								}
+							}else {
+								if (val <= 0) { // se è negativo lo poni a zero
+									map[x+i][y+j].cost = 0;
+									for (int k = 0; k < map[x+i][y+j].numAirRoute; k++) {
+										map[x+i][y+j].array[k].costAirRoute = 0;
 									}
 								}
 							}
 						}
-						//gestione incremento altezza delle colonne
-						if (i <= (raggio/2)) {
-							if (hColo < (raggio - 1)) {
-								hColo = hColo + 2;
-							}else if (hColo < raggio){
-								hColo = hColo + 1;
-							}
-						}else {
-							if (hColo == raggio && raggio % 2 != 0) {
-								hColo = hColo - 1;
-							}else if (hColo > 0) {
-								hColo = hColo - 2;
-							}
-						}
 					}
-					printf("OK\n");
 				}
-				else {
-					printf("KO\n");
-				}
-				break;
-			case 'toggle_air_route':
-				int flag = 0; //flag per la ricerca della rotta da inserire
-				int x_start = 0, y_start = 0, x_end = 0, y_end = 0;
-				res = scanf("%d" "%d" "%d" "%d", x_start, y_start, &x_end, &y_end);
-
-				if (x_start <= row && x_end <= row && y_end <= col && y_end <= col) {
-					AirRoute *airRouteHead = map[x_start][y_start].array;
-					AirRoute *airRoutePrev = NULL;
-
-					while (airRouteHead != NULL || flag == 1) {
-						if (airRouteHead->xDest == x_end && airRouteHead->yDest == y_end) {
-							flag = 1;
-						} else {
-							airRoutePrev = airRouteHead;
-							airRouteHead = airRouteHead->next;
-						}
+				//gestione incremento altezza delle colonne
+				if (i <= (raggio/2)) {
+					if (hColo < (raggio - 1)) {
+						hColo = hColo + 2;
+					}else if (hColo < raggio){
+						hColo = hColo + 1;
 					}
-
-					//se lo trova, lo rimuove
-					if (flag == 1 && airRoutePrev != NULL) {
-						airRoutePrev->next = airRouteHead->next;
-						map[x_start][y_start].numAirRoute--;
-						printf("OK\n");
-					} else if (flag == 1 && airRoutePrev == NULL) {
-						map[x_start][y_start].array = airRouteHead->next;
-						map[x_start][y_start].numAirRoute--;
-						printf("OK\n");
-					}
-					//altrimenti lo aggiunge solo se ce ne sono meno di 5
-					else if (map[x_start][y_start].numAirRoute < 5) {
-						AirRoute *airRouteNew = malloc(sizeof(AirRoute));
-						airRouteNew->xDest = x_end;
-						airRouteNew->yDest = y_end;
-						airRouteNew->costAirRoute = AirRouteCost(map, x_start, y_start);
-						map[x_start][y_start].numAirRoute++;
-						airRouteNew->next = map[x_start][y_start].array;
-						map[x_start][y_start].array = airRouteNew;
-						printf("OK\n");
-					} else {
-						printf("KO\n");
-					}
-
-					free(airRoutePrev);
-					free(airRouteHead);
-				} else {
-					printf("KO\n");
-				}
-
-				break;
-			case 'travel_cost':
-				int xp, yp, xd, yd;
-				res = scanf("%d" "%d" "%d" "%d", &xp, &yp, &xd, &yd);
-
-				if (xp < 0 || yp < 0 || xd < 0 || yd < 0 || xp > row || yp > col || xd > row || yd > col) {
-					printf("KO\n");
 				}else {
-					if (map[xp][yp].cost == 0) {
-						printf("-1\n");
+					if (hColo == raggio && raggio % 2 != 0) {
+						hColo = hColo - 1;
+					}else if (hColo > 0) {
+						hColo = hColo - 2;
 					}
-					else if (xp == xd && yp == yd) {
-						printf("0\n");
-					}
-					else {
-						//dijkstra
-						Heap *heap[row*col];
-						int heapSize = 0;
-
-						//! manca da assegnare i nodi allo heap
-					}
-
 				}
-				break;
-			default:
-				printf("Wrong command.\n");
+			}
+			printf("OK\n");
 		}
+		else {
+			printf("KO\n");
+		}
+	}
+	else if (strcmp(command, "toggle_air_route") == 0) {
+		int flag = 0; //flag per la ricerca della rotta da inserire
+		int x_start = 0, y_start = 0, x_end = 0, y_end = 0;
+		scanf("%d" "%d" "%d" "%d", &x_start, &y_start, &x_end, &y_end);
+
+		if (x_start <= row && x_end <= row && y_end <= col && y_end <= col) {
+			AirRoute *airRouteHead = map[x_start][y_start].array;
+			AirRoute *airRoutePrev = NULL;
+
+			while (airRouteHead != NULL && flag != 1) {
+				if (airRouteHead->xDest == x_end && airRouteHead->yDest == y_end) {
+					flag = 1;
+				} else {
+					airRoutePrev = airRouteHead;
+					airRouteHead = airRouteHead->next;
+				}
+			}
+
+			//se lo trova, lo rimuove
+			if (flag == 1 && airRoutePrev != NULL) {
+				airRoutePrev->next = airRouteHead->next;
+				map[x_start][y_start].numAirRoute--;
+				printf("OK\n");
+			} else if (flag == 1 && airRoutePrev == NULL) {
+				map[x_start][y_start].array = airRouteHead->next;
+				map[x_start][y_start].numAirRoute--;
+				printf("OK\n");
+			}
+			//altrimenti lo aggiunge solo se ce ne sono meno di 5
+			else if (map[x_start][y_start].numAirRoute < 5) {
+				AirRoute *airRouteNew = malloc(sizeof(AirRoute));
+				airRouteNew->xDest = x_end;
+				airRouteNew->yDest = y_end;
+				airRouteNew->costAirRoute = AirRouteCost(map, x_start, y_start);
+				map[x_start][y_start].numAirRoute++;
+				airRouteNew->next = map[x_start][y_start].array;
+				map[x_start][y_start].array = airRouteNew;
+				printf("OK\n");
+			} else {
+				printf("KO\n");
+			}
+
+			free(airRoutePrev);
+			free(airRouteHead);
+		} else {
+			printf("KO\n");
+		}
+	}
+	else if (strcmp(command, "travel_cost") == 0) {
+		int xp, yp, xd, yd;
+		res = scanf("%d" "%d" "%d" "%d", &xp, &yp, &xd, &yd);
+
+		if (xp < 0 || yp < 0 || xd < 0 || yd < 0 || xp > row || yp > col || xd > row || yd > col) {
+			printf("KO\n");
+		}else {
+			if (map[xp][yp].cost == 0) {
+				printf("-1\n");
+			}
+			else if (xp == xd && yp == yd) {
+				printf("0\n");
+			}
+			else {
+				//dijkstra
+				DijkstraShortestPath(map, map[xp][yp].cost, xd * col + yd, col, row);
+			}
+
+		}
+	}
+
 	}
 	free(map);
 }
@@ -317,11 +312,130 @@ int Parent(int i) {
 
 //Gestione dell'inserimento nello heap
 void HeapInsert (Heap *Q, int *heapSize, int key) {
-	(*heapSize) ++;
 	//uso -1 al posto di infinito perchè i pesi vanno da 0 a 100,usare un numero come 101 sarebbe errato perchè
 	//procedenndo con i passi potrei avere la somma maggiore. Ma mai negativa
 	Q[*heapSize].key = -1;
+	(*heapSize) ++;
 	DecreaseKey(Q, *heapSize, key);
 }
 
-//! manca in codice effettivo dell'algoritmo di dijkstra
+void DijkstraShortestPath(Tile **G, int w, int id, int col, int row) {
+	int heapSize = 0, distance[row][col];
+	Heap Q[row*col];
+	Heap s;
+	s.id = id;
+	s.key = 0;
+	s.prev = NULL;
+
+	HeapInsert (&Q, &heapSize, s.key);
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			if ((i*col)+j != s.id) {
+				Q[(i*col)+j].key = -1;
+				Q[(i*col)+j].prev = NULL;
+				Q[(i*col)+j].id = (i*col)+j;
+				HeapInsert (&Q, &heapSize, Q[(i*col)+j].key);
+			}
+		}
+	}
+
+	while (Q != NULL) {
+		Heap u;
+		u = ExtractMin (Q, &heapSize); //! segmentation fault: probabilmente non gestisce bene il -1 come infinito
+		int x = u.id / col;
+		int y = u.id % col;
+
+		int xp, yp, zp;
+		xp = y - ((x - (x & 1)) / 2);
+		zp = x;
+		yp = xp- zp;
+
+		int xd, yd, zd, idFinale;
+
+		//1 0 -1
+		xd = xp + 1;
+		yd = yp;
+		zd = zp - 1;
+		idFinale = (xd * col) + (zp -(zp & 1)) /2;
+		if (Q[idFinale].key > u.key + w) {
+			Q[idFinale].key = u.key + w;
+			Q[idFinale].prev = u.id;
+			distance[zd][(xp -(zp & 1)) /2] = u.key + w;
+			DecreaseKey (&Q, idFinale, Q[idFinale].key);
+		}
+
+		//1 -1 0
+		xd = xp + 1;
+		yd = yp -1;
+		zd = zp;
+		idFinale = (zd * col) + xp + (zp -(zp & 1)) /2;
+		if (Q[idFinale].key > u.key + w) {
+			Q[idFinale].key = u.key + w;
+			Q[idFinale].prev = u.id;
+			distance[zd][xp + (zp -(zp & 1)) /2] = u.key + w;
+			DecreaseKey (&Q, idFinale, Q[idFinale].key);
+		}
+
+		//0 -1 1
+		xd = xp;
+		yd = yp - 1;
+		zd = zp + 1;
+		idFinale = (zd * col) + xp + (zp -(zp & 1)) /2;
+		if (Q[idFinale].key > u.key + w) {
+			Q[idFinale].key = u.key + w;
+			Q[idFinale].prev = u.id;
+			distance[zd][xp + (zp -(zp & 1)) /2] = u.key + w;
+			DecreaseKey (&Q, idFinale, Q[idFinale].key);
+		}
+
+		//-1 0 1
+		xd = xp - 1;
+		yd = yp;
+		zd = zp + 1;
+		idFinale = (zd * col) + xp + (zp -(zp & 1)) /2;
+		if (Q[idFinale].key > u.key + w) {
+			Q[idFinale].key = u.key + w;
+			Q[idFinale].prev = u.id;
+			distance[zd][xp + (zp -(zp & 1)) /2] = u.key + w;
+			DecreaseKey (&Q, idFinale, Q[idFinale].key);
+		}
+
+		//-1 1 0
+		xd = xp - 1;
+		yd = yp + 1;
+		zd = zp;
+		idFinale = (zd * col) + xp + (zp -(zp & 1)) /2;
+		if (Q[idFinale].key > u.key + w) {
+			Q[idFinale].key = u.key + w;
+			Q[idFinale].prev = u.id;
+			distance[zd][xp + (zp -(zp & 1)) /2] = u.key + w;
+			DecreaseKey (&Q, idFinale, Q[idFinale].key);
+		}
+
+		//0 1 -1
+		xd = xp;
+		yd = yp + 1;
+		zd = zp - 1;
+		idFinale = (zd * col) + xp + (zp -(zp & 1)) /2;
+		if (Q[idFinale].key > u.key + w) {
+			Q[idFinale].key = u.key + w;
+			Q[idFinale].prev = u.id;
+			distance[zd][xp + (zp -(zp & 1)) /2] = u.key + w;
+			DecreaseKey (&Q, idFinale, Q[idFinale].key);
+		}
+
+		//air route
+		for (int i = 0; i < G[x][y].numAirRoute; i++) {
+			idFinale = G[x][y].array->xDest * col + G[x][y].array->yDest;
+			if (Q[idFinale].key > u.key + w) {
+				Q[idFinale].key = u.key + w;
+				Q[idFinale].prev = u.id;
+				distance[zd][xp + (zp -(zp & 1)) /2] = u.key + w;
+				DecreaseKey (&Q, idFinale, Q[idFinale].key);
+			}
+		}
+
+	}
+	printf("%d\n", distance[id/col][id%col]);
+}
